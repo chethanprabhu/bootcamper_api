c83c5fae6b374452ead132867ada74b2114c4dc1
Chethan Prabhu <chethanprabhu65@gmail.com>
2022-03-25 00:23:11 +0530

    added comments

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 //Loading the env variables
+//if the .env file is in root then we don't need to pass any aruguement to config method.
+//to read anything from our env file we need to install dotenv package.
+//like ex to read anything from .properties file, we may need to install PropertiesReader package.
 dotenv.config({path: "./config/config.env"});
 
 const app = express();
